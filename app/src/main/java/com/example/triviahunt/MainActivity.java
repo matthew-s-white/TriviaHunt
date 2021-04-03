19bbe4c139baf75f2780ad6980b6d9ac9fa7d1d6
matthew-s-white <white753@purdue.edu>
2021-04-03 18:15:42 -0400

    Finally got current location (shows Mountain View in emulator, check if works on tangible Android device). Additionally added a random number of markers to represent trivia questions within your current area.

--- app/src/main/java/com/example/triviahunt/MainActivity.java
+++ app/src/main/java/com/example/triviahunt/MainActivity.java
@@ -34,65 +34,90 @@ import com.google.android.gms.tasks.Task;
 
 import static com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
 
     private SupportMapFragment supportMapFragment;
+    Location currLocation;
     private FusedLocationProviderClient client;
+    private static final int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.google_map);
+
         client = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getCurrLocation();
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 44);
-        }
+        getCurrLocation();
     }
 
     private void getCurrLocation() {
-        @SuppressLint("MissingPermission") Task<Location> task = client.getLastLocation();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+
+        Task<Location> task = client.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+
                 if (location != null) {
-                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            System.out.println("LATITUDE: " + location.getLatitude() + " LONGITUDE: " + location.getLongitude());
-                            LatLng currLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            MarkerOptions currLoc = new MarkerOptions().position(currLatLng).title("Me");
-                            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                                // TODO: Consider calling
-                                //    ActivityCompat#requestPermissions
-                                // here to request the missing permissions, and then overriding
-                                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                //                                          int[] grantResults)
-                                // to handle the case where the user grants the permission. See the documentation
-                                // for ActivityCompat#requestPermissions for more details.
-                                return;
-                            }
-                            googleMap.setMyLocationEnabled(true);
-                            googleMap.addMarker(currLoc);
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLng, 10));
-                        }
-                    });
+                    currLocation = location;
+                    supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.google_map);
+
+                    supportMapFragment.getMapAsync(MainActivity.this);
                 }
             }
         });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng currLatLng = new LatLng(currLocation.getLatitude(), currLocation.getLongitude());
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(currLatLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLng, 20));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        generateNearbyMarkers(googleMap);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 44){
+        if (requestCode == REQUEST_CODE){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 getCurrLocation();
             }
+        }
+    }
+
+    private void generateNearbyMarkers(GoogleMap googleMap){
+        double start_latitude = 37.4220186;
+        double start_longitude = -122.0839727;
+        int num_of_marks;
+        num_of_marks = (int)((Math.random() * 5.0) + 4.0);
+        System.out.println("num_of_marks: "+ num_of_marks);
+        for(int i = 0; i < num_of_marks; i++){
+            double new_latitude, new_longitude;
+            new_latitude = ((((Math.random() * 2) - 1)) * (Math.random() / 800)) + start_latitude;
+            new_longitude = ((((Math.random() * 2) - 1)) * (Math.random() / 800)) + start_longitude;
+            System.out.println("new_lat: " + new_latitude + " new_long: " + new_longitude);
+            LatLng newLatLng = new LatLng(new_latitude, new_longitude);
+            MarkerOptions newLoc = new MarkerOptions().position(newLatLng).title("TrivaQ" + i);
+            googleMap.addMarker(newLoc);
         }
     }
 }
