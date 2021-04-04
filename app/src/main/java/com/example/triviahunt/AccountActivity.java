0f5aab22fecde31a802b355596a171fee28a101e
Arunsc2 <asenchou@purdue.edu>
2021-04-03 23:38:32 -0400

    Created xml files and accounts.txt

---
+++ app/src/main/java/com/example/triviahunt/AccountActivity.java
@@ -0,0 +1,20 @@
+package com.example.triviahunt;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class AccountActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+
+    public void createAccount(View view){
+       // TODO
+    }
+
+}
