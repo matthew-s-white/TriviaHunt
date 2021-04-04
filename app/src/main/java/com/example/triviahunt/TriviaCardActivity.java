cf5c7293f19fd2f00b122ef6d477ef23b0c179a4
ssankha <ssankha@purdue.edu>
2021-04-03 23:50:20 -0400

    Added trivia card

---
+++ app/src/main/java/com/example/triviahunt/TriviaCardActivity.java
@@ -0,0 +1,67 @@
+package com.example.triviahunt;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class TriviaCardActivity extends AppCompatActivity {
+    public int counter;
+    public boolean choiceSelected = false;
+    @TargetApi(Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView counttime=findViewById(R.id.timer);
+
+        CountDownTimer cd = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if(!choiceSelected) {
+                    counttime.setText(String.valueOf(counter));
+                    counter++;
+                }
+            }
+            @Override
+            public void onFinish() {
+                finish();
+            }
+        }.start();
+    }
+
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.answer1 || view.getId() == R.id.answer2 || view.getId() == R.id.answer3 || view.getId() == R.id.answer4) {
+            choiceSelected = true;
+
+            switch(view.getId()) {
+                case R.id.answer1:
+                    findViewById(R.id.answer1).setBackgroundColor(2263842);
+                    break;
+
+                case R.id.answer2:
+                    findViewById(R.id.answer2).setBackgroundColor(2263842);
+                    break;
+
+                case R.id.answer3:
+                    findViewById(R.id.answer3).setBackgroundColor(2263842);
+                    break;
+
+                case R.id.answer4:
+                    findViewById(R.id.answer4).setBackgroundColor(2263842);
+                    break;
+            }
+        }
+    }
+}
