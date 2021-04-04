ad13160c7f77413de8bf51ead041bb9408465018
Arunsc2 <asenchou@purdue.edu>
2021-04-04 00:49:04 -0400

    Create Account v1

--- app/src/main/java/com/example/triviahunt/AccountActivity.java
+++ app/src/main/java/com/example/triviahunt/AccountActivity.java
@@ -1,7 +1,9 @@
 package com.example.triviahunt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -11,10 +13,17 @@ public class AccountActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
-    }
 
-    public void createAccount(View view){
-       // TODO
+        Button createAnAccount = (Button) findViewById(R.id.CreateAccount);
+
+        createAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AccountActivity.this, CreateAccount.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/triviahunt/CreateAccount.java
@@ -0,0 +1,54 @@
+package com.example.triviahunt;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.FragmentActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateAccount extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_account_details);
+
+        Button createAccountButton = (Button) findViewById(R.id.CreateAccount);
+
+        final EditText usernameET = (EditText) findViewById(R.id.editUsername);
+        usernameET.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        final EditText nameET = (EditText) findViewById(R.id.editName);
+        nameET.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        final EditText passwordET = (EditText) findViewById(R.id.editTextTextPassword2);
+        passwordET.setInputType(InputType.TYPE_CLASS_TEXT);
+
+
+        createAccountButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String username = usernameET.getText().toString();
+                String name = nameET.getText().toString();
+                String password = passwordET.getText().toString();
+
+                Account newAccount = new Account();
+                newAccount.setUsername(username);
+                newAccount.setName(name);
+                newAccount.setPassword(password);
+
+                try {
+                    newAccount.writeToFile("accounts.txt"); // Might need to change (include filepath)
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
